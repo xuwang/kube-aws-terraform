7d24edc96577bf2136d758c84bba39c0a5b71d5d
Xu Wang <xuwang@gmail.com>
2017-02-12 11:02:18 -0800

    For etcd, use its envs.sh to override capacity

--- resources/etcd/envs.sh
+++ resources/etcd/envs.sh
@@ -2,3 +2,10 @@
 
 export MODULE=etcd
 export TF_VAR_module=${MODULE}
+
+# Override default values to make sure we have odd number of etcd servers
+export TF_VAR_cluster_min_size=1
+export TF_VAR_cluster_max_size=1
+export TF_VAR_cluster_desired_capacity=1
+export TF_VAR_coreos_update_channel=${COREOS_UPDATE_CHANNEL}
+export TF_VAR_instance_type=t2.medium
