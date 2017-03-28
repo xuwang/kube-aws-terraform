5b773a3706b1a9fd9c93affabe08127d906192e8
Xueshan Feng <xueshan.feng@gmail.com>
2017-03-27 23:05:54 -0700

    set COREOS_PUBLIC_IPV4 and COREOS_PRIVATE_IPV4 from metadata

--- resources/vault/artifacts/upload/setup.sh
+++ resources/vault/artifacts/upload/setup.sh
@@ -1,26 +1,33 @@
 #!/bin/bash -e
 
 # vault script
+mkdir -p /opt/etc/vault
 cp -r ./scripts /opt/etc/vault
 chmod 755 /opt/etc/vault/scripts/*.sh
+
+# Get instance private and public IPs
+export COREOS_PRIVATE_IPV4=$(curl -s 169.254.169.254/latest/meta-data/local-ipv4)
+export COREOS_PUBLIC_IPV4=$(curl -s 169.254.169.254/latest/meta-data/public-ipv4)
 
 GET=/opt/bin/s3get.sh
 
 # install vault binary
 $GET ${CONFIG_BUCKET} vault/envvars /opt/etc/vault/envvars
-source /opt/etc/vault/envvars /etc/environment
+source /opt/etc/vault/envvars
 docker run --rm -v /opt/bin:/tmp ${VAULT_IMAGE} cp /bin/vault /tmp/vault
 
 # downlaod vault configuration files
 $GET ${CONFIG_BUCKET} vault/vault.sh  /etc/profile.d/vault.sh
-$GET ${CONFIG_BUCKET} vault/vault.hcl /opt/etc/vault/vault.hcl
-$GET ${CONFIG_BUCKET} vault/vault.cnf /opt/etc/vault/certs/vault.cnf.tmpl
+$GET ${CONFIG_BUCKET} vault/vault.hcl /opt/etc/vault/vault.hcl.tmpl;
+cat /opt/etc/vault/vault.hcl.tmpl | envsubst > /opt/etc/vault/vault.hcl
 
 # download vault root ca/key
-$GET  ${CONFIG_BUCKET} pki/ca-key.pem /opt/etc/vault/ca/ca-key.pem
-$GET  ${CONFIG_BUCKET} pki/ca.pem /opt/etc/vault/ca/ca.pem
+$GET ${CONFIG_BUCKET} pki/ca-key.pem /opt/etc/vault/ca/ca-key.pem
+$GET ${CONFIG_BUCKET} pki/ca.pem /opt/etc/vault/ca/ca.pem
 
 # generate vault certs
+mkdir -p /opt/etc/vault/certs
 cd /opt/etc/vault/certs
-cat vault.cnf.tmp | envsubst > vault.cnf
+$GET ${CONFIG_BUCKET} vault/vault.cnf vault.cnf.tmpl
+cat vault.cnf.tmpl | envsubst > vault.cnf
 /opt/etc/vault/scripts/gen-vault-cert.sh
