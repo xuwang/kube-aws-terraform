2459ca829f43904e3ed74480b5d9b2e65d146234
Xueshan Feng <xueshan.feng@gmail.com>
2017-04-08 23:43:41 -0700

    For kube nodes, generate separate kubelet and kube-proxy cert for RBAC.

--- resources/node/artifacts/upload/get-certs.sh
+++ resources/node/artifacts/upload/get-certs.sh
@@ -42,18 +42,21 @@ fi
 
 # Vault PKI Token. We store them in both /etc/etcd/certs and /var/lib/kubernetes directories
 export VAULT_TOKEN=$token
-vault write -format=json \
-  ${CLUSTER_NAME}/pki/$issuer_name/issue/$issuer_name common_name=kubelet \
-  alt_names="kubernetes.default,*.cluster.local" \
-  ttl=43800h0m0s \
-  ip_sans="127.0.0.1,$COREOS_PRIVATE_IPV4" >  $issuer_name-bundle.certs
+for i in kubelet kube-proxy
+do
+    vault write -format=json \
+      ${CLUSTER_NAME}/pki/$issuer_name/issue/$issuer_name common_name=$i \
+      alt_names="kubernetes.default,*.cluster.local" \
+      ttl=43800h0m0s \
+      ip_sans="127.0.0.1,$COREOS_PRIVATE_IPV4" >  $issuer_name-bundle.certs
 
-if [ ! -s $issuer_name-bundle.certs ]; then
-  echo "$issuer_name-bundle.certs doesn't exist or has zero size."
-  exit 1
-fi
+    if [ ! -s $issuer_name-bundle.certs ]; then
+      echo "$issuer_name-bundle.certs doesn't exist or has zero size."
+      exit 1
+    fi
 
-mkdir -p $install_path
-cat $issuer_name-bundle.certs | jq -r ".data.certificate" > $install_path/$issuer_name.pem
-cat $issuer_name-bundle.certs | jq -r ".data.private_key" > $install_path/$issuer_name-key.pem
-cat $issuer_name-bundle.certs | jq -r ".data.issuing_ca" > $install_path/$issuer_name-ca.pem
+    mkdir -p $install_path
+    cat $issuer_name-bundle.certs | jq -r ".data.certificate" > $install_path/${i}.pem
+    cat $issuer_name-bundle.certs | jq -r ".data.private_key" > $install_path/${i}-key.pem
+    cat $issuer_name-bundle.certs | jq -r ".data.issuing_ca" > $install_path/$issuer_name-ca.pem
+done
