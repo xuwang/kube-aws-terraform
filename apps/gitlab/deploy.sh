0edb145c8f37c77ddd5880d5faeb1c8cb5592fc8
Xueshan Feng <xueshan-feng@stanford.edu>
2017-04-19 19:28:09 -0700

    better test elb ready logic.

--- apps/gitlab/deploy.sh
+++ apps/gitlab/deploy.sh
@@ -29,30 +29,25 @@ kubectl get rc
 echo "Service:"
 kubectl get svc
 
-if ! which -s kubectl; then
-  echo "kubectl command not installed"
-  exit 1
-fi
-
 # Wait for the Pods to be ready
 while ! kubectl get pods -o json -l name=gitlab   |grep ready | grep -q true ;
 do
   echo "Waiting for GitLab to be ready..."
-  sleep 2
+  sleep 10
 done
 
-elb_dns=""
-while [ "X$elb_dns" = "X" ];
+elb_dns=$(kubectl get svc gitlab  -o json | jq -r '.status.loadBalancer.ingress[0].hostname')
+while ! echo $elb_dns | grep -q 'elb.amazonaws.com'
 do
-  echo "Waiting for loadBanlancer..."
+  echo "Getting loadBanlancer dns name..."
   elb_dns=$(kubectl get svc gitlab  -o json | jq -r '.status.loadBalancer.ingress[0].hostname')
-  sleep 10
+  sleep 15
 done
 
 while ! curl -L -I $elb_dns | grep -v "200 OK" ;
 do
-   echo "Waiting for loadBanlancer..."
-   sleep 10
+   echo "Waiting for loadBanlancer 200 OK"
+   sleep 15
 done
 echo "Conntect to demo GitLab at: http://$elb_dns"
 open "http://$elb_dns"
