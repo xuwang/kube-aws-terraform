1f8b6d82c4ed5e50eb6ce1f6d4e6df6c5b188bbe
Xu Wang <xuwang@gmail.com>
2017-04-29 19:48:42 -0700

    Added confirmation before destroying route53 zone.

--- route53/tf/main.tf
+++ route53/tf/main.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_zone" "public" {
     name = "${var.route53_zone_name}"
-
+    force_destroy = "${var.route53_zone_force_destroy}"
     tags {
         Name = "${var.route53_zone_name}"
     }
--- route53/tf/variables.tf
+++ route53/tf/variables.tf
@@ -1,13 +1,11 @@
-data "aws_caller_identity" "current" { }
-
-variable "cluster_internal_zone" {
-    default = "cluster.internal"
-}
-
 variable "module_name" {
     default = "undefined"
 }
 
 variable "route53_zone_name" {
     default = "example.com"
+}
+
+variable "route53_zone_force_destroy" {
+    default  = "false"
 }
