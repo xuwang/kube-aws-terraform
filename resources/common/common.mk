d4088fffd47d2fdf9435eb639fb53ab7747d3bb8
Xu Wang <xuwang@gmail.com>
2017-08-30 10:55:04 -0700

    Moved .PHONY to the top of a target.

--- resources/common/common.mk
+++ resources/common/common.mk
@@ -72,6 +72,7 @@ help: ## this info
 	@cat Makefile | grep -E '^[a-zA-Z_-]+:.*?## .*$$' | \
 		awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}' | sort -k1,1
 
+.PHONY: update-build
 update-build: check-profile check-route53-zone ## create or update build dir for terraform
 	@mkdir -p ${BUILD_DIR}
 # copy modules to buid dir
@@ -89,12 +90,14 @@ update-build: check-profile check-route53-zone ## create or update build dir for
 # generate tf provider file
 	@../scripts/gen-provider.sh > ${BUILD_DIR}/${TF_PROVIDER}
 
+.PHONY: check-profile
 check-profile: ## validate AWS profile
 	@if ! aws --profile ${AWS_PROFILE} sts get-caller-identity --output text --query 'Account'> /dev/null 2>&1 ; then \
 		echo "ERROR: AWS profile \"${AWS_PROFILE}\" is not setup!"; \
 		exit 1 ; \
 	fi
 
+.PHONY: check-route53-zone
 check-route53-zone:  ## validate AWS route53 zone
 	@if ! aws --profile ${AWS_PROFILE} route53 list-hosted-zones --output json | \
 			jq -r --arg ROUTE53_ZONE_NAME "${ROUTE53_ZONE_NAME}" '.HostedZones[] | \
@@ -103,59 +106,61 @@ check-route53-zone:  ## validate AWS route53 zone
 		exit 1; \
 	fi
 
+.PHONY: plan apply list show output
 plan: init ## terraform plan
 	@${TF_PLAN}
-
 apply: init ## terraform apply
 	@${TF_APPLY}
-
 list: init ## terraform list
 	@${TF_LIST}
-
 show: init ## terraform show
 	@${TF_SHOW}
-
 output: ## terraform output
 	@${TF_OUTPUT}
 
+.PHONY: destroy-plan show-destroy-plan
 destroy-plan: init ## terraform destroy-plan
 	@echo Plan destroy ${MODULE}...
 	@-${TF_DESTROY_PLAN} -out ${TF_VAR_build_dir}/destroy-${MODULE}.plan
-
 show-destroy-plan: ## show resources planed to be destroyed
 	@-${TF_SHOW} ${TF_VAR_build_dir}/destroy-${MODULE}.plan | grep -- -
 
+.PHONY: destroy
 destroy: init ## terraform destroy
 	@echo Destroy ${MODULE}...
 	@${TF_DESTROY}
 	$(MAKE) clean
 
+.PHONY: refresh
 refresh: init ## terraform refresh
 	@${TF_REFRESH}
 
+.PHONY: clean
 clean: ## delete build dir
 	rm -rf ${BUILD_DIR}
 
+.PHONY: create-key destroy-key
 create-key: ## create AWS keypair for this module
 	../scripts/aws-keypair.sh -c $(CLUSTER_NAME)-${MODULE};
-
 destroy-key: ## destroy AWS keypair for this module
 	../scripts/aws-keypair.sh -d $(CLUSTER_NAME)-${MODULE};
 
+.PHONY: remote-ssh open-ssh close-ssh
 remote-ssh: open-ssh ## Run remote ssh
 	@$(MAKE) get-ips
 	@echo "For all systemd logs, run ssh core@<ip> journalctl -f "
 	@echo "For a secific service, run ssh core@<ip> journalctl -f -u <kube-apiserver>|kube-controller-manager|kubelet|kube-proxy"
 	@echo "To revoke firewall rule: make close-ssh"
-
 open-ssh:
 	@../scripts/allow-myip.sh -a ${MODULE} 22
 close-ssh:
 	@../scripts/allow-myip.sh -r ${MODULE} 22
 
+.PHONY: get-ips
 get-ips: ## get ips of EC2 in this module
 	@echo "${MODULE}; public ips: " `$(SCRIPTS)/get-ec2-public-id.sh $(CLUSTER_NAME)-${MODULE}`
 
+.PHONY: upload-artifacts
 # Call this explicitly to upload scripts to s3
 upload-artifacts: check-profile ## upload artifacts to S3 bucket
 	@if [ -d "$(PWD)/artifacts/upload" ]; \
@@ -169,6 +174,7 @@ upload-artifacts: check-profile ## upload artifacts to S3 bucket
 		@echo "$(PWD)/artifacts/upload doesn't exit. Nothing to upload"; \
 	fi
 
+.PHONY: upload-config
 # Call this explicitly to upload scripts to s3
 upload-config: check-profile  ## upload files in artifacts/upload to S3
 	@if [ -d "$(PWD)/artifacts/upload" ]; \
@@ -182,6 +188,7 @@ upload-config: check-profile  ## upload files in artifacts/upload to S3
 		@echo "$(PWD)/artifacts/upload doesn't exit. Nothing to upload"; \
 	fi
 
+.PHONY: init
 init: sync-docker-time update-build ## setup terraform remote state
 	@echo set remote state to s3://${TF_REMOTE_STATE_BUCKET}/${TF_REMOTE_STATE_PATH}
 
@@ -201,16 +208,27 @@ init: sync-docker-time update-build ## setup terraform remote state
 # Terraform remote S3 backend init
 	@${TF_INIT}
 
+.PHONY: remote-push
 remote-push: init ## terraform remote push
-	@${TF_CMD} remote push
+	@echo "Update remote state from a local state file."
+	@$(MAKE) confirm
+	@${TF_CMD} state push
 
+.PHONY: remote-pull
 remote-pull: init ## terraform remote pull
-	@${TF_CMD} remote pull
+	@${TF_CMD} state pull
 
+.PHONY: remote-cmd
+remote-cmd: open-ssh ## Run remote shell command
+	@ip=`make get-ips | awk '{print $$NF}'`; \
+		ssh -A core@$$ip "$(filter-out $@,$(MAKECMDGOALS))"
+
+.PHONY: ssh
 ssh: open-ssh ## ssh into a node
 	@ip=`make get-ips | awk '{print $$NF}'`; ssh -A core@$$ip
 	@../scripts/allow-myip.sh -r ${MODULE} 22
 
+.PHONY: force-destroy-remote
 force-destroy-remote: update-build  ## destroy terraform remote state bucket
 	@if aws s3 --profile ${AWS_PROFILE} --region ${TF_REMOTE_STATE_REGION} ls s3://${TF_REMOTE_STATE_BUCKET}  &> /dev/null; \
 	then \
@@ -220,6 +238,7 @@ force-destroy-remote: update-build  ## destroy terraform remote state bucket
 			--force ; \
 	fi
 
+.PHONY: upgrade-kube
 upgrade-kube: ## upgrade k8s to version defined in TF_VAR_kube_version
 	@echo "Will upgrade ${MODULE}'s Kubernetes to ${TF_VAR_kube_version}."
 	@$(MAKE) plan
@@ -227,17 +246,17 @@ upgrade-kube: ## upgrade k8s to version defined in TF_VAR_kube_version
 	@$(MAKE) apply
 	@echo "Don't forget to reboot ${MODULE}s."
 
+.PHONY: sync-docker-time
 # see https://github.com/docker/for-mac/issues/17#issuecomment-236517032
 sync-docker-time: ## sync docker vm time with hardware clock
 	@docker run --rm --privileged alpine hwclock -s
 
+.PHONY: confirm
 confirm:
 	@echo "CONTINUE? [Y/N]: "; read ANSWER; \
 	if [ ! "$$ANSWER" = "Y" ]; then \
 		echo "Exiting." ; exit 1 ; \
 	fi
 
-
-.PHONY: init plan apply show output destroy clean remote-push remote-pull help
-.PHONY: update-build update-ami update_user-data get-ips sync-docker-time
-.PHONY: create-key upload-key destroy-key force-destroy-remote
+.DEFAULT:
+	@#echo no target $@.
