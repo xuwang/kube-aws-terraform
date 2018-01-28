43cdad6ee5e4dce0078582c4bfe4e4c7b0442f57
Xueshan Feng <xueshan.feng@gmail.com>
2018-01-28 09:24:48 -0800

    removed socat installation. CoreOS now install it.

--- resources/master/envs.sh
+++ resources/master/envs.sh
@@ -1,3 +1,9 @@
 # Env for master
 export MODULE=master
 export TF_VAR_module_name=${MODULE}
+
+# Override default values
+export TF_VAR_cluster_min_size=3
+export TF_VAR_cluster_max_size=3
+export TF_VAR_cluster_desired_capacity=3
+export TF_VAR_instance_type=t2.medium
