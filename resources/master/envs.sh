eda6be99dbacf16dce111036ac3adef9d2af35e7
Xueshan Feng <xueshan.feng@gmail.com>
2018-01-29 15:14:21 -0800

    default master changed from 3 to 1.

--- resources/master/envs.sh
+++ resources/master/envs.sh
@@ -3,7 +3,7 @@ export MODULE=master
 export TF_VAR_module_name=${MODULE}
 
 # Override default values
-export TF_VAR_cluster_min_size=3
+export TF_VAR_cluster_min_size=1
 export TF_VAR_cluster_max_size=3
-export TF_VAR_cluster_desired_capacity=3
+export TF_VAR_cluster_desired_capacity=1
 export TF_VAR_instance_type=t2.medium
